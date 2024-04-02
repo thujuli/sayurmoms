d6ba07b02310457b29882023384b699e07db7711
ronaldj220 <65749850+ronaldj220@users.noreply.github.com>
2024-04-02 16:48:29 +0700

    New categories (#16)
    * feat: Making Navbar and Jumbotron no Slider
    
    * Fix: Jumbotron not sliding when tts desktop
    
    * "Fix: categories"
    
    * Fixing Jumbotron and Benefit
    
    ---------
    
    Co-authored-by: I Putu Julianta <95893131+thujuli@users.noreply.github.com>

---
+++ helper/index.ts
@@ -0,0 +1,60 @@
+// Import Image Logo
+import img from "../public/images/Logo Sayurmoms-responsive.png";
+
+// Import group image for benefits
+import apple from "../public/images/Apple.png";
+import shopping from "../public/images/Group.png";
+import money from "../public/images/Group 1231.png";
+import verified from "../public/images/Group 1233.png";
+
+// Navigation links
+export const nav_link = [
+  {
+    href: "/",
+    label: "Beranda",
+  },
+  {
+    href: "/",
+    label: "Produk Unggulan",
+  },
+  {
+    href: "/",
+    label: "Tentang Kami",
+  },
+  {
+    href: "/",
+    label: "Produk",
+  },
+  {
+    href: "/",
+    label: "Ulasan",
+  },
+  {
+    href: "/",
+    label: "FAQ",
+  },
+];
+// Kasih gambar disini
+export const logo = img;
+
+// Benefit
+export const benefits = [
+  {
+    img: apple,
+    desc: "Segar dan Berkualitas",
+  },
+  {
+    img: shopping,
+    desc: "Pengiriman Cepat",
+  },
+  {
+    img: money,
+    desc: "Pesan Mudah, Harga Hemat",
+  },
+  {
+    img: verified,
+    desc: "Lengkap dan Terpercaya",
+  },
+];
+
+// Code lainnya selain dari contentful atau yg manual masukkan di Index
