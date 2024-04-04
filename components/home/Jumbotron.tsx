8cdcc726363acebd5b6361986608c9e56e8b3e41
ronaldj220 <ronjuliostagjhs21@gmail.com>
2024-04-04 22:18:29 +0700

    fix: fixing pesan sekarang and design for items pesan sekarang

--- components/home/Jumbotron.tsx
+++ components/home/Jumbotron.tsx
@@ -12,9 +12,14 @@ import { useMediaQuery } from "@/hooks/use-media-query";
 import whatsapp from "@/public/images/Vector.png";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
-import { ShoppingBasket } from "lucide-react";
+import { Globe, ShoppingBasket } from "lucide-react";
 import { CarouselData, CategoryData } from "@/lib/types";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+
+import shopee from "@/public/images/shopee.png";
+import tokopedia from "@/public/images/tokopedia.png";
+import Link from "next/link";
+
 
 interface IJumbotronProps {
   carousel?: CarouselData[];
@@ -22,6 +27,10 @@ interface IJumbotronProps {
 
 const Jumbotron: React.FunctionComponent<IJumbotronProps> = (props) => {
   const { carousel } = props;
+  const [dropdownVisible, setDropdownVisible] = React.useState<boolean>(false);
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
 
   const isDesktop = useMediaQuery("(min-width: 1024px)");
   const [jumbotron, setJumbotron] = React.useState<any[]>([]);
@@ -78,7 +87,7 @@ const Jumbotron: React.FunctionComponent<IJumbotronProps> = (props) => {
         (val, idx) =>
           idx === 0 && (
             <div key={idx} className="absolute top-[-70px] right-[5px]">
-              <div className="relative top-[500px] right-16 cursor-pointer">
+              <Link className="fixed top-[500px] right-16" href='https://api.whatsapp.com/'>
                 <Image
                   src={whatsapp}
                   alt="WhatsApp Icon"
@@ -86,20 +95,64 @@ const Jumbotron: React.FunctionComponent<IJumbotronProps> = (props) => {
                   height={124}
                   className="border-8 border-solid border-white rounded-[50%] bg-gradient-to-r from-[#25CF43] to-[#61FD7D]"
                 />
-              </div>
+              </Link>
             </div>
           )
       )}
       {/* Gunakan untuk Pesan Sekarang */}
-      <div className="absolute top-[200px] right-[-65px] transform -rotate-90 flex justify-center items-center bg-[#232323] text-[#F8F8F8] font-bold">
+      <div className="fixed top-[200px] right-[-65px] transform -rotate-90 flex justify-center items-center bg-[#232323] text-[#F8F8F8] font-bold cursor-pointer" onClick={toggleDropdown}>
         <span>
           <ShoppingBasket className="mr-2" />
         </span>
         <span>Pesan Sekarang</span>
       </div>
+      {dropdownVisible && (
+        <div className="fixed top-[135px] right-[25px] px-2 py-5 shadow-md z-10 bg-[#F3F6EA] rounded-l-xl">
+          <div className="block">
+            <a
+              href="https://shopee.co.id/sayurmoms.id"
+              className="block mb-2 w-[119px]"
+              target="_blank"
+            >
+              <Image
+                src={shopee}
+                alt="Shopee Logo"
+                width={119}
+                height={46}
+              />
+            </a>
+          </div>
+          <div className="block">
+            <a
+              href="https://www.tokopedia.com/sayurmoms"
+              className="block mb-2 w-[127px]"
+              target="_blank"
+            >
+              <Image
+                src={tokopedia}
+                alt="Tokopedia Logo"
+                width={127}
+                height={32}
+                className="-ml-1 w-24 h-8"
+              />
+            </a>
+          </div>
+          <div className="block">
+            <a
+              href="https://sayurmoms.take.app/"
+              target="_blank"
+            >
+              <div className="flex items-center">
+                <Globe color="#909090" className="mr-1" />
+                <span className="text-[16px] text-center tracking-[0.4px]">sayurmoms.take.app</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   ) : (
-    <div className="overflow-hidden relative top-[40px]" ref={emblaRef}>
+    <div className="overflow-hidden relative top-[30px]" ref={emblaRef}>
       <div className="embla_container flex relative z-0">
         {jumbotron.map((val: any, index) => (
           <div className="flex-[0_0_100%] min-w-0 rounded-b-3xl" key={index}>
@@ -112,13 +165,6 @@ const Jumbotron: React.FunctionComponent<IJumbotronProps> = (props) => {
                 className="w-full rounded-b-3xl"
               />
             </AspectRatio>
-            <a href={val.link} target="_blank">
-              <button
-                className={`relative z-0 bottom-[18%] left-[8%] bg-[#232323] text-white px-2 py-1 rounded-[20px] text-[10px]`}
-              >
-                Cek Sekarang
-              </button>
-            </a>
           </div>
         ))}
       </div>
